07bf086b44b9f052d557cb9c26ad7cf4a675f32e
harley-ghostie <harleyvpn99@proton.me>
2026-04-29 17:15:26 -0300

    Create gerar_targets_relay.sh

---
+++ gerar_targets_relay.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+RANGE="10.50.103.0/24"
+TARGETS_FILE="/tmp/relay_targets.txt"
+
+echo "[+] Escaneando rede $RANGE com crackmapexec..."
+crackmapexec smb $RANGE | grep "Signing: False" | cut -d " " -f 1 | sort -u | while read ip; do
+    echo "smb://$ip"
+done > "$TARGETS_FILE"
+
+echo "[+] Arquivo de alvos criado em: $TARGETS_FILE"
+echo
+cat "$TARGETS_FILE"
+echo
+echo "[+] Pronto pra rodar com ntlmrelayx 😈"
